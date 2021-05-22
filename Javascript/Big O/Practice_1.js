63e18e3567365e718d9008f0472702949887536b
Coder--Shona <>
2021-05-22 20:15:54 +0000

    two example added

--- Javascript/Big O/Practice_1.js
+++ Javascript/Big O/Practice_1.js
@@ -1,19 +1,28 @@
 // Learning Big O
 
-const nemos = ['nemos'];
+// const nemos = ['nemos'];
+
+// function findNemos(array) {
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] === 'nemo') {
+//       console.log('We had found NEMOS!!!');
+//     }
+//   }
+// }
+
+// findNemos(nemos);
+
+const nemos_TWO = ['nemos_TWO'];
 
 function findNemos(array) {
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === 'nemo') {
+  for (let i = 0; 0 < array.length; i++) {
+    if ('nemos_TWO' === 'nemos_TWO') {
       console.log('We had found NEMOS!!!');
     }
   }
 }
 
-findNemos(nemos);
-
-
-
+findNemos(nemos_TWO);
 
 
 
