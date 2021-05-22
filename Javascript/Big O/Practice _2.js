edf251ec6027040e8ebe0fd60707c26c9b8f6ca3
Coder--Shona <>
2021-05-22 20:32:53 +0000

    learned big O and scalability part 1 still some left

--- Javascript/Big O/Practice _2.js
+++ Javascript/Big O/Practice _2.js
@@ -1,5 +1,28 @@
 // Writing BIG O & Scalability :
 
+// const nemos = ['nemos'];
+// const fruits = ['strawberry','pineapple','apricot','pears','pomegranate','lychee','mango','banana','orange','apple'];
+
+// function findNemos(array) {
+//   let y0 = performance.now();
+
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] === 'nemos') {
+//       console.log('We had found NEMOS!!!');
+//     }
+//   }
+//   let y1 = performance.now();
+//   console.log('Calling to find Nemos ' + (y1-y0) + ' miliseconds');
+// }
+
+// // findNemos(nemos);
+// findNemos(fruits);
+
+
+// example 2:
+
+
+
 
 
 
--- index.js
+++ index.js
@@ -1,31 +1,21 @@
-// Learning Big O
+const nemos = ['nemos'];
+const fruits = ['strawberry','pineapple','apricot','pears','pomegranate','lychee','mango','banana','orange','apple'];
 
-// const nemos = ['nemos'];
-
-// function findNemos(array) {
-//   for (let i = 0; i < array.length; i++) {
-//     if (array[i] === 'nemo') {
-//       console.log('We had found NEMOS!!!');
-//     }
-//   }
-// }
-
-// findNemos(nemos);
-
-const nemos_TWO = ['nemos_TWO'];
+const larger = new Array(100).fill('nemos');
 
 function findNemos(array) {
-  for (let i = 0; 0 < array.length; i++) {
-    if ('nemos_TWO' === 'nemos_TWO') {
+  let y0 = performance.now();
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 'nemos') {
       console.log('We had found NEMOS!!!');
     }
   }
+  let y1 = performance.now();
+  console.log('Calling to find Nemos ' + (y1-y0) + ' miliseconds');
 }
 
-findNemos(nemos_TWO);
-
-
-
-
-
+// findNemos(nemos);
+// findNemos(fruits);
+findNemos(larger);
 
