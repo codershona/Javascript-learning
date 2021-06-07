03eeac16c0f802e09fbde5a46539c02e3188a35a
Coder--Shona <>
2021-06-07 18:43:25 +0000

    implementing an array and using strings and arrays

--- Javascript/ARRAYS/array.js
+++ Javascript/ARRAYS/array.js
@@ -162,6 +162,80 @@ wizard1.introduce();
 // Implementing An Array :
 
 
+// Implementing An Array :
+// const a = [];
+// creating an array using JS
+
+// creating a class
+
+class MyArray {
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+
+  get(index) {
+    return this.data [index]
+  }
+
+  // push method
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return this.length;
+  }
+
+  // adding pop item to remove or delete the last item of the array
+  pop() {
+    // here we do not need to pass any parameter inside pop
+    const lastItem = this.data[this.length-1];
+    delete this.data[this.length-1];
+    this.length--;
+    return lastItem;
+
+  }
+
+  // delete method
+  delete(index) {
+    const item = this.data[index];
+    this.shiftItems(index);
+     return item;
+  }
+ 
+  // shiftitems will be a method of our array
+  shiftItems(index) {
+   for (let i = index; i < this.length - 1; i++) {
+     this.data[i] = this.data[i+1];
+   }
+    delete this.data[this.length - 1];
+    this.length--;
+  }
+
+}
+
+const newArray = new MyArray();
+newArray.push("hello");
+newArray.push("yours");
+
+// adding a new item to the array
+ newArray.push("!!!!");
+// newArray.pop();
+// newArray.pop();
+// newArray.delete(1);
+newArray.delete(0);
+// newArray.delete('1');
+newArray.push('were');
+newArray.push('awesome');
+newArray.delete(1);
+console.log(newArray);
+// console.log(newArray.get(0));
+
+
+
+// Strings and Arrays :
+// Reverse A String:
+
+
 
 
 
--- index.js
+++ index.js
@@ -1,95 +1,71 @@
+// Implementing An Array :
+// const a = [];
+// creating an array using JS
 
-// CLASSES IN JAVASCRIPT:
+// creating a class
 
-// talk about reference type, context and instantiation
+class MyArray {
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
 
-// reference type
+  get(index) {
+    return this.data [index]
+  }
 
-// [] === []
+  // push method
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return this.length;
+  }
 
-// [1] === [1]
+  // adding pop item to remove or delete the last item of the array
+  pop() {
+    // here we do not need to pass any parameter inside pop
+    const lastItem = this.data[this.length-1];
+    delete this.data[this.length-1];
+    this.length--;
+    return lastItem;
 
+  }
 
-var object1 = { value: 10 }; // false
+  // delete method
+  delete(index) {
+    const item = this.data[index];
+    this.shiftItems(index);
+     return item;
+  }
+ 
+  // shiftitems will be a method of our array
+  shiftItems(index) {
+   for (let i = index; i < this.length - 1; i++) {
+     this.data[i] = this.data[i+1];
+   }
+    delete this.data[this.length - 1];
+    this.length--;
+  }
 
-var object2 = object1; // false
-
-var object3 = { value: 10 }; // false
-
-object2 === object1; // true
-
-object1 === object3; // false
-
-object1.value = 15; // 15
-
-object2.value // 15
-
-object3.value // 10
-
-// context vs scope
-
-// function b() {
-//   let a = 4;
-// }
-
-console.log(this);
-
-console.log(this === window); //true
-
-this.alert("hello worlds");
-
-window.alert()
-alert()
-
-function a() {
-  console.log(this);
 }
 
-a();  // shows window objects
+const newArray = new MyArray();
+newArray.push("hello");
+newArray.push("yours");
 
-window.a();
+// adding a new item to the array
+ newArray.push("!!!!");
+// newArray.pop();
+// newArray.pop();
+// newArray.delete(1);
+newArray.delete(0);
+// newArray.delete('1');
+newArray.push('were');
+newArray.push('awesome');
+newArray.delete(1);
+console.log(newArray);
+// console.log(newArray.get(0));
 
-
-const object4 = {
-  a: function() {
-    console.log(this);
-  }
-}
-
-object4.a();
-
-
-// Instantiation
-
-class Player {
-  constructor(name, type) {
-    // adding this object
-    console.log('player', this);
-    this.name = name;
-    this.type = type;
-  }
-  introduce() {
-    console.log(`HI this is ${this.name}, Classes in ${this.type}`);
-  }
-}
-
-
-class Wizard extends Player {
-  constructor(name, type) {
-    // adding this object
-    super(name, type)
-    console.log('wizard', this);
-    // super(name, type)
-  }
-  play() {
-    console.log(`We are here ${this.type}`);
-  }
-}
-
-const wizard1 = new Wizard('Jelly', 'Hina');
-const wizard2 = new Wizard('John', 'Hena');
-wizard1.play();
-wizard1.introduce();
 
 
 
