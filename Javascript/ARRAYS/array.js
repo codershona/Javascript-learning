63950db44ec0acddf072c08c02772c44c5ff38c1
Coder--Shona <>
2021-06-08 14:51:17 +0000

    interview question 1 reverse a string using arrays

--- Javascript/ARRAYS/array.js
+++ Javascript/ARRAYS/array.js
@@ -235,6 +235,48 @@ console.log(newArray);
 // Strings and Arrays :
 // Reverse A String:
 
+// Most important Interview question:
+// reverse a string
+
+
+function reverse(str) {
+  // check input 
+  if(!str || str.length < 2 || typeof str !== 'string') {
+    return 'that is not good';
+  } 
+
+  const backwards = [];
+  const totalItems = str.length - 1;
+  for (let i = totalItems; i >= 0; i--) {
+    backwards.push(str[i]);
+    // str.split()
+  }
+  console.log(backwards);
+
+  // return backwards.toString
+  return backwards.join('');
+}
+
+
+function reverseTwo(str) {
+  return str.split('').reverse().join('');
+}
+
+
+// create one line function by using es6
+// const reverseThree = str => str.split('').reverse().join('');
+
+const reverseThree = str => [...str].reverse().join('');
+// reverse('I am learning Data Structures Arrays')
+// reverseTwo('I am learning Data Structures Arrays')
+reverseThree('I am learning Data Structures Arrays')
+
+
+
+// Most important Interview question:
+// Merge Sorted Arrays:
+
+
 
 
 
--- index.js
+++ index.js
@@ -1,71 +1,37 @@
-// Implementing An Array :
-// const a = [];
-// creating an array using JS
+// reverse a string
 
-// creating a class
 
-class MyArray {
-  constructor() {
-    this.length = 0;
-    this.data = {};
+function reverse(str) {
+  // check input 
+  if(!str || str.length < 2 || typeof str !== 'string') {
+    return 'that is not good';
+  } 
+
+  const backwards = [];
+  const totalItems = str.length - 1;
+  for (let i = totalItems; i >= 0; i--) {
+    backwards.push(str[i]);
+    // str.split()
   }
+  console.log(backwards);
 
-  get(index) {
-    return this.data [index]
-  }
-
-  // push method
-  push(item) {
-    this.data[this.length] = item;
-    this.length++;
-    return this.length;
-  }
-
-  // adding pop item to remove or delete the last item of the array
-  pop() {
-    // here we do not need to pass any parameter inside pop
-    const lastItem = this.data[this.length-1];
-    delete this.data[this.length-1];
-    this.length--;
-    return lastItem;
-
-  }
-
-  // delete method
-  delete(index) {
-    const item = this.data[index];
-    this.shiftItems(index);
-     return item;
-  }
- 
-  // shiftitems will be a method of our array
-  shiftItems(index) {
-   for (let i = index; i < this.length - 1; i++) {
-     this.data[i] = this.data[i+1];
-   }
-    delete this.data[this.length - 1];
-    this.length--;
-  }
-
+  // return backwards.toString
+  return backwards.join('');
 }
 
-const newArray = new MyArray();
-newArray.push("hello");
-newArray.push("yours");
 
-// adding a new item to the array
- newArray.push("!!!!");
-// newArray.pop();
-// newArray.pop();
-// newArray.delete(1);
-newArray.delete(0);
-// newArray.delete('1');
-newArray.push('were');
-newArray.push('awesome');
-newArray.delete(1);
-console.log(newArray);
-// console.log(newArray.get(0));
+function reverseTwo(str) {
+  return str.split('').reverse().join('');
+}
 
+
+// create one line function by using es6
+// const reverseThree = str => str.split('').reverse().join('');
+
+const reverseThree = str => [...str].reverse().join('');
+// reverse('I am learning Data Structures Arrays')
+// reverseTwo('I am learning Data Structures Arrays')
+reverseThree('I am learning Data Structures Arrays')
 
 
 
