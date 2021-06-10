5e36faf53a44f8f3271d0b9e95329f3409b10f3d
Coder--Shona <57604500+codershona@users.noreply.github.com>
2021-06-10 11:30:09 +0000

    rotate array (medium) problem solved

--- Javascript/ARRAYS/Rotate_Array.js
+++ Javascript/ARRAYS/Rotate_Array.js
@@ -5,10 +5,37 @@
  * @param {number} k
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var rotate = function(nums, k) {
-    
+// function rotate (nums, k) {  
+//     // using for loop method
+//     for (let e = 0; e < k; e++) {
+//         nums.unshift(nums.pop());
+//     }    
+// };
+// time limit exceed
+
+function rotate (nums, k) {  
+    let arrayItem = 0;
+    const array_sub_Item = nums.length;
+    k %= array_sub_Item;    
+    // using for loop method
+    for (let e = 0; e < k; e++) {
+        // nums.unshift(nums.pop());
+        arrayItem = nums.pop();
+        nums.unshift(arrayItem);
+    }    
 };
 
+
+
+
+// OR,
+
+var rotate = function(nums, k) {
+    for(let i=0; i<k; i++){
+        nums.unshift(nums.splice(nums.length-1, 1)[0])
+    }
+    return nums;
+};
 
 
 
