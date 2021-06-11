847485b35e117296cb9063df551f5ebd80f1a21c
Coder--Shona <57604500+codershona@users.noreply.github.com>
2021-06-11 16:49:45 +0000

    hash collisions

--- Javascript/HASHTABLES/hashtable.js
+++ Javascript/HASHTABLES/hashtable.js
@@ -1,4 +1,19 @@
 // Data Structure: Hash Table
+// hash collisions:
+let user = {
+  age: 25,
+  name: "Jenny",
+  magic: true,
+  scream: function() {
+    console.log('learning hashtables');
+  }
+}
 
+user.age // O(1)
+user.spell = 'learning to codes are interesting'; // O(1)
+//user
+user.scream(); // O(1)
+
+// O(n/k)
 
 
--- index.js
+++ index.js
@@ -1,54 +1,15 @@
-// creating a fucntion whhere array1 is first parameter and array2 is second parameter
-function mergeSortedArrrays(array1, array2) {
-   const mergedArray = [];
-   let array1Item = array1[0];
-   let array2Item = array2[0];
-   let i = 1;
-   let j = 1;
-
-   // Check Input 
-   if(array1.length === 0) {
-     return array2;
-   }
-
-   if (array2.length === 0) {
-     return array1;
-   }
-
-   // using while loop or for loop
-   // while loop
-   while (array1Item || array2Item) {
-     console.log(array1Item, array2Item);
-      // if (array1Item < array2Item) {
-        if (!array2Item || array1Item < array2Item) {
-          // if(array2Item === undefined || array1Item < array2Item){
-      //  if (!undefined || array1Item < array2Item) {        mergedArray.push(array1Item)
-        // array1Item = array1[1]
-        array1Item = array1[i];
-        i++;
-      } else {
-        mergedArray.push(array2Item);
-        array2Item = array2[j];
-        j++;
-      }
-   }
-
-
-   return mergedArray;
+let user = {
+  age: 25,
+  name: "Jenny",
+  magic: true,
+  scream: function() {
+    console.log('learning hashtables');
+  }
 }
-// undefined < 6
-// console.log('answer', undefined < 6);
-// mergeSortedArrrays([0,3,4,31], [4,6,30]);
-mergeSortedArrrays([0,3,4], [4,6,30]);
 
+user.age // O(1)
+user.spell = 'learning to codes are interesting'; // O(1)
+//user
+user.scream(); // O(1)
 
-// mergeSortedArrrays([0,3,4,31], []);
-
-
-
-
-
-
-
-// mergeSortedArrrays([0,3,4,31], [4,6,30]);
-
+// O(n/k)
