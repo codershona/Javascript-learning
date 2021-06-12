43f0eacff425eb9f08c63118bb62e6f7aea1168e
Coder--Shona <57604500+codershona@users.noreply.github.com>
2021-06-12 15:24:02 +0000

    linked list examples

--- Javascript/Linked Lists/linked_lists.js
+++ Javascript/Linked Lists/linked_lists.js
@@ -1,1 +1,10 @@
 // linked lists
+const basket = ['apples', 'grapes', 'pears']
+
+linked list: apples --> grapes --> pears
+
+apples
+8947 --> grapes
+          8742 --> pears
+                    372 --> null
+
