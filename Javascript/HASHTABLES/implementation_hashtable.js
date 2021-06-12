06bb62706710554be11b82ab7852f36ce3b35927
Coder--Shona <57604500+codershona@users.noreply.github.com>
2021-06-12 14:18:28 +0000

    implementation of our own hash tables 1

--- Javascript/HASHTABLES/implementation_hashtable.js
+++ Javascript/HASHTABLES/implementation_hashtable.js
@@ -1,5 +1,6 @@
 // implementation of our own hash tables:
 
+
 class HashTable {
   constructor(size){
     this.data = new Array(size);
@@ -8,15 +9,66 @@ class HashTable {
   _hash(key) {
     let hash = 0;
     for (let i =0; i < key.length; i++){
-        hash = (hash + key.charCodeAt(i) * i) % this.data.length
+        hash = (hash + key.charCodeAt(i) * i) %
+        this.data.length
+        console.log(hash);
     }
     return hash;
+  } // this is very fast because it is O(1)
+
+  // creating set method 
+   set(key, value) {
+      let address = this._hash(key);
+      if (!this.data[address]) {
+        this.data[address] = [];
+        // this.data[address].push([key, value]);
+        // console.log(this.data);
+      // } else {
+      //   this.data[address].push([key, value]);
+      // }
+         } 
+        this.data[address].push([key, value]);
+        return this.data;
+  }  // O(1)
+
+  // creating get method
+  get(key) {
+    let address = this._hash(key);
+    const currentBuckets = this.data[address];
+    // console.log(currentBuckets);
+    // if (currentBuckets.length) {
+      if (currentBuckets) {
+     // using for loop
+     for(let i = 0; i < currentBuckets.length; i++) {
+       if(currentBuckets[i][0] === key) {
+         return currentBuckets[i][1];
+       }
+      }
+    } // O(1)
+    return undefined;
+
   }
 }
 
-const myHashTable = new HashTable(50);
+// const myHashTable = new HashTable(50);
+const myHashTable = new HashTable(2);
+
+// set methods:
+// myHashTable.set('grape', 10000)
+// myHashTable.set('grapess', 10000)
 myHashTable.set('grapes', 10000)
-myHashTable.get('grapes')
-myHashTable.set('apples', 9)
+// myHashTable.set('apples', 9)
+myHashTable.set('apples', 55)
+// myHashTable.set('apple', 15)
+
+// get methods:
+// myHashTable.get('grapes')
 myHashTable.get('apples')
+
+// _hash methods:
+// myHashTable._hash('grapes');
+
+
+
+
 
--- index.js
+++ index.js
@@ -1,16 +1,69 @@
-let user = {
-  age: 25,
-  name: "Jenny",
-  magic: true,
-  // scream: 
-  scream: function() {
-    console.log('learning hashtables');
+
+class HashTable {
+  constructor(size){
+    this.data = new Array(size);
+  }
+
+  _hash(key) {
+    let hash = 0;
+    for (let i =0; i < key.length; i++){
+        hash = (hash + key.charCodeAt(i) * i) %
+        this.data.length
+        console.log(hash);
+    }
+    return hash;
+  } // this is very fast because it is O(1)
+
+  // creating set method 
+   set(key, value) {
+      let address = this._hash(key);
+      if (!this.data[address]) {
+        this.data[address] = [];
+        // this.data[address].push([key, value]);
+        // console.log(this.data);
+      // } else {
+      //   this.data[address].push([key, value]);
+      // }
+         } 
+        this.data[address].push([key, value]);
+        return this.data;
+  }  // O(1)
+
+  // creating get method
+  get(key) {
+    let address = this._hash(key);
+    const currentBuckets = this.data[address];
+    // console.log(currentBuckets);
+    // if (currentBuckets.length) {
+      if (currentBuckets) {
+     // using for loop
+     for(let i = 0; i < currentBuckets.length; i++) {
+       if(currentBuckets[i][0] === key) {
+         return currentBuckets[i][1];
+       }
+      }
+    } // O(1)
+    return undefined;
+
   }
 }
 
-user.age // O(1)
-user.spell = 'learning to codes are interesting'; // O(1)
-//user
-user.scream(); // O(1)
+// const myHashTable = new HashTable(50);
+const myHashTable = new HashTable(2);
 
-// O(n/k)
+// set methods:
+// myHashTable.set('grape', 10000)
+// myHashTable.set('grapess', 10000)
+myHashTable.set('grapes', 10000)
+// myHashTable.set('apples', 9)
+myHashTable.set('apples', 55)
+// myHashTable.set('apple', 15)
+
+// get methods:
+// myHashTable.get('grapes')
+myHashTable.get('apples')
+
+// _hash methods:
+// myHashTable._hash('grapes');
+
+
