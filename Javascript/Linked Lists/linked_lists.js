91dcecb97e96fcc043c29ddf1b9e5af7d3f128b4
Coder--Shona <57604500+codershona@users.noreply.github.com>
2021-06-13 14:34:05 +0000

    pointer problem solved

--- Javascript/Linked Lists/linked_lists.js
+++ Javascript/Linked Lists/linked_lists.js
@@ -1,10 +1,28 @@
 // linked lists
-const basket = ['apples', 'grapes', 'pears']
+// const basket = ['apples', 'grapes', 'pears']
 
-linked list: apples --> grapes --> pears
+// linked list: apples --> grapes --> pears
 
-apples
-8947 --> grapes
-          8742 --> pears
-                    372 --> null
+// apples
+// 8947 --> grapes
+//           8742 --> pears
+//                     372 --> null
+
+// This is called Pointer
+// const obj1 = { a: true };
+// const obj2 = obj1;
+
+
+// console.log('1', obj1);
+// console.log('2',obj2);
+
+let obj1 = { a: true };
+let obj2 = obj1;
+obj1.a = 'buyaa';  // this is called pointer
+delete obj1
+obj2 = 'hello'
+// console.log('1', obj1);
+console.log('2',obj2);
+
+
 
