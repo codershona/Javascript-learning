ac032bd6640cd4272c60073c11d8b7b889d76683
Coder--Shona <57604500+codershona@users.noreply.github.com>
2021-06-13 16:07:35 +0000

    linkedlist insert() and remove() method done

--- Javascript/Linked Lists/linkedlist_insert.js
+++ Javascript/Linked Lists/linkedlist_insert.js
@@ -1,7 +1,7 @@
 // insert() method:
 // 1-->10-->99-->5-->16
 // add a method insert() to the linked list that adds a node to the specified index.
-
+// Linked Lists Implementation
 class LinkedList {
   constructor(value) {
     this.head = {
@@ -37,13 +37,54 @@ class LinkedList {
     let currentNode = this.head;
     while(currentNode !== null){
         array.push(currentNode.value)
-        currentNode = currentNode.next
+        currentNode = currentNode.next;     
+      
     }
     return array;
   }
   // insert() method uses
   insert(index, value){
+    //Check for proper parameters;
     //Code here
+    // return this.printList();
+        // if (index === 0) {
+      //   this.prepend(value);
+      //   return this.printList();
+      // }
+        // check params
+        if (index >= this.length) {
+         return this.append(value);
+        }
+        const newNode = {
+          value: value,
+          next: null
+      };
+      const leader = this.traverseToIndex(index-1);
+      const holdingPointer = leader.next;
+      leader.next = newNode;
+      newNode.next = holdingPointer;
+      this.length++;
+      return this.printList();
+  }
+  traverseToIndex(index) {
+    // check params
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+  // delete or remove() method
+  remove(index) {
+    // index is a parameter
+    // code here
+    // check params 
+    const leader = this.traverseToIndex(index - 1);
+    const unwantedNode = leader.next;
+    leader.next = unwantedNode.next;
+    this.length--;
     return this.printList();
   }
 }
@@ -52,10 +93,15 @@ let myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
 myLinkedList.prepend(1);
+myLinkedList.printList();
+// myLinkedList.printList();
 // insert() method uses
-myLinkedList.insert(2, 99)
-myLinkedList.insert(20, 88)
-
-
-
+// myLinkedList.insert(200, 99);
+myLinkedList.insert(2, 99);
+// myLinkedList.printList();
+myLinkedList.insert(20, 88);
+myLinkedList.printList();
+// remove() method
+myLinkedList.remove(2);
+myLinkedList.printList(2);
 
