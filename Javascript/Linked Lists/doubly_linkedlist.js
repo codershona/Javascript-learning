1faeea000e8592791bccca0b4861ccf6b5d524c8
Coder--Shona <57604500+codershona@users.noreply.github.com>
2021-06-13 16:55:49 +0000

    doubly linked list problem solved

--- Javascript/Linked Lists/doubly_linkedlist.js
+++ Javascript/Linked Lists/doubly_linkedlist.js
@@ -2,12 +2,14 @@
 // doubly linked lists allow us to traverse our list in backwards
 // creating your own doubly linked list
 // add a method remove() to the linked list that deletes a node to the specified index.
-
-class LinkedList {
+// Data Structure: Doubly Linked List Implementation
+// converting singly linked list into doubly linked lists
+class DoublyLinkedList {
   constructor(value) {
     this.head = {
       value: value,
-      next: null
+      next: null,
+      prev: null
     };
     this.tail = this.head;
     this.length = 1;
@@ -15,19 +17,23 @@ class LinkedList {
   append(value) {
     const newNode = {
       value: value,
-      next: null
-    }
+      next: null,
+      prev: null
+    };
+    newNode.prev = this.tail;
     this.tail.next = newNode;
     this.tail = newNode;
     this.length++;
     return this;
-  }
+  };
   prepend(value) {
     const newNode = {
       value: value,
-      next: null
-    }
+      next: null,
+      prev: null
+    };
     newNode.next = this.head;
+    this.head.prev = newNode;
     this.head = newNode;
     this.length++;
     return this;
@@ -44,19 +50,23 @@ class LinkedList {
   insert(index, value){
     //Check for proper parameters;
     if(index >= this.length) {
-      console.log('yes')
+      // console.log('yes')
       return this.append(value);
-    }
-    
+    } 
     const newNode = {
       value: value,
-      next: null
-    }
+      next: null,
+      prev: null
+    };
     const leader = this.traverseToIndex(index-1);
-    const holdingPointer = leader.next;
+    // const holdingPointer = leader.next;
+    const follower = leader.next;
     leader.next = newNode;
-    newNode.next = holdingPointer;
+    newNode.prev = leader;
+    newNode.next = follower;
+    follower.prev = newNode;
     this.length++;
+    console.log(this);
     return this.printList();
   }
   traverseToIndex(index) {
@@ -79,10 +89,14 @@ class LinkedList {
   }
 }
 
-let myLinkedList = new LinkedList(10);
+let myLinkedList = new DoublyLinkedList(10);
 myLinkedList.append(5);
-myLinkedList.append(16);myLinkedList.prepend(1);
+myLinkedList.append(16);
+myLinkedList.prepend(1);
+myLinkedList.printList();
 myLinkedList.insert(2, 99);
-myLinkedList.insert(20, 88);
-myLinkedList.remove(2);
-
+// myLinkedList.insert(20, 88);
+// myLinkedList.remove(2);
+// myLinkedList.printList()
+// myLinkedList.reverse()
+// console.log(myLinkedList);
--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-// insert() method:
-// 1-->10-->99-->5-->16
-// add a method insert() to the linked list that adds a node to the specified index.
-// Linked Lists Implementation
-class LinkedList {
+// Doubly Linked Lists
+// doubly linked lists allow us to traverse our list in backwards
+// creating your own doubly linked list
+// add a method remove() to the linked list that deletes a node to the specified index.
+// Data Structure: Doubly Linked List Implementation
+// converting singly linked list into doubly linked lists
+class DoublyLinkedList {
   constructor(value) {
     this.head = {
       value: value,
-      next: null
+      next: null,
+      prev: null
     };
     this.tail = this.head;
     this.length = 1;
@@ -14,74 +17,71 @@ class LinkedList {
   append(value) {
     const newNode = {
       value: value,
-      next: null
-    }
+      next: null,
+      prev: null
+    };
+    newNode.prev = this.tail;
     this.tail.next = newNode;
     this.tail = newNode;
     this.length++;
     return this;
-  }
+  };
   prepend(value) {
     const newNode = {
       value: value,
-      next: null
-    }
+      next: null,
+      prev: null
+    };
     newNode.next = this.head;
+    this.head.prev = newNode;
     this.head = newNode;
     this.length++;
     return this;
   }
-  // printlist() function method
   printList() {
     const array = [];
     let currentNode = this.head;
     while(currentNode !== null){
         array.push(currentNode.value)
-        currentNode = currentNode.next;     
-      
+        currentNode = currentNode.next
     }
     return array;
   }
-  // insert() method uses
   insert(index, value){
     //Check for proper parameters;
-    //Code here
-    // return this.printList();
-        // if (index === 0) {
-      //   this.prepend(value);
-      //   return this.printList();
-      // }
-        // check params
-        if (index >= this.length) {
-         return this.append(value);
-        }
-        const newNode = {
-          value: value,
-          next: null
-      };
-      const leader = this.traverseToIndex(index-1);
-      const holdingPointer = leader.next;
-      leader.next = newNode;
-      newNode.next = holdingPointer;
-      this.length++;
-      return this.printList();
+    if(index >= this.length) {
+      // console.log('yes')
+      return this.append(value);
+    } 
+    const newNode = {
+      value: value,
+      next: null,
+      prev: null
+    };
+    const leader = this.traverseToIndex(index-1);
+    // const holdingPointer = leader.next;
+    const follower = leader.next;
+    leader.next = newNode;
+    newNode.prev = leader;
+    newNode.next = follower;
+    follower.prev = newNode;
+    this.length++;
+    console.log(this);
+    return this.printList();
   }
   traverseToIndex(index) {
-    // check params
+    //Check parameters
     let counter = 0;
     let currentNode = this.head;
-    while (counter !== index) {
+    while(counter !== index){
       currentNode = currentNode.next;
       counter++;
     }
     return currentNode;
   }
-  // delete or remove() method
   remove(index) {
-    // index is a parameter
-    // code here
-    // check params 
-    const leader = this.traverseToIndex(index - 1);
+    // Check Parameters      
+    const leader = this.traverseToIndex(index-1);
     const unwantedNode = leader.next;
     leader.next = unwantedNode.next;
     this.length--;
@@ -89,19 +89,14 @@ class LinkedList {
   }
 }
 
-let myLinkedList = new LinkedList(10);
+let myLinkedList = new DoublyLinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
 myLinkedList.prepend(1);
 myLinkedList.printList();
-// myLinkedList.printList();
-// insert() method uses
-// myLinkedList.insert(200, 99);
 myLinkedList.insert(2, 99);
-// myLinkedList.printList();
-myLinkedList.insert(20, 88);
-myLinkedList.printList();
-// remove() method
-myLinkedList.remove(2);
-myLinkedList.printList(2);
-
+// myLinkedList.insert(20, 88);
+// myLinkedList.remove(2);
+// myLinkedList.printList()
+// myLinkedList.reverse()
+// console.log(myLinkedList);
