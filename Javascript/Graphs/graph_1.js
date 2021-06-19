a3dea46b4fbca2364d250feac03e5db47ee10bfb
Coder--Shona <57604500+codershona@users.noreply.github.com>
2021-06-19 14:24:07 +0000

    studying graphs

---
+++ Javascript/Graphs/graph_1.js
@@ -0,0 +1,21 @@
+// Graph
+
+// Edge List 
+const graph = [[0, 2], [2, 3], [2, 1], [1, 3]];
+
+// Adjacent Lists
+const graph = [[2], [2, 3], [0, 1, 3], [1, 2]];
+
+// to represent graphs last methods
+// Adjacent Matrix
+const graph = [
+  [0, 0, 1, 0],
+  [0, 0, 1, 1],
+  [1, 1, 0, 1],
+  [0, 1, 1, 0]
+]
+
+
+
+
+
