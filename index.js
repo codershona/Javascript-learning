244a5723837f1815b74c513cd7d68edee411d0ce
Coder--Shona <57604500+codershona@users.noreply.github.com>
2021-06-25 15:24:40 +0000

    Selection Sort implementation

--- index.js
+++ index.js
@@ -1,27 +1,11 @@
-// Bubble Sort:
-// bubble sort is one fo the simplest sorting algorithm and it is least efficient
-// what do you think the time complexity and space complexity of the sorting algorithm?
-// 
+// Selection Sort:
+// time complexity O(n^2) or O(n)
+// space complexity O(1)
+
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
-function bubbleSort(array) {
-  const length = array.length;
-
-  // for loop method
-  for (let i = 0; i < length; i++) {
-    for ( let j = 0; j < length; j++) {
-      if (array[j] > array[j + 1]) {
-        // swap numbers
-        // let tempory_variable = array[i];
-        let tempory_variable = array[j];
-        array[j] = array[j + 1];
-        // array[j + 1] = array[j];
-         array[j + 1] = tempory_variable;
-      }
-    }
-  }
+function selectionSort(array) {
+  
 }
 
-bubbleSort(numbers);
-console.log(numbers);
-
+selectionSort(numbers);
