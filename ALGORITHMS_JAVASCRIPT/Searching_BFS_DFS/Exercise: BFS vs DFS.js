7b1be2890e866c0bfd5ae56eb012aafdb3e1fbba
Coder--Shona <57604500+codershona@users.noreply.github.com>
2021-07-01 10:28:16 +0000

    BFS vs DFS problem solved

---
+++ ALGORITHMS_JAVASCRIPT/Searching_BFS_DFS/Exercise: BFS vs DFS.js
@@ -0,0 +1,34 @@
+//If you know a solution is not far from the root of the tree:
+
+//If the tree is very deep and solutions are rare: 
+
+//If the tree is very wide:
+
+//If solutions are frequent but located deep in the tree:
+
+//Determining whether a path exists between two nodes:
+
+//Finding the shortest path:
+
+
+SOLUTION:
+//If you know a solution is not far from the root of the tree:
+BFS
+
+//If the tree is very deep and solutions are rare, 
+BFS (DFS will take long time. )
+
+//If the tree is very wide:
+DFS (BFS will need too much memory)
+
+//If solutions are frequent but located deep in the tree
+DFS
+
+//determining whether a path exists between two nodes
+DFS
+
+//Finding the shortest path
+BFS
+
+
+
