26577fd009534d4297e91cff5605c233bb1f6ff1
Coder--Shona <57604500+codershona@users.noreply.github.com>
2021-07-30 20:19:50 +0000

    Best Time to Buy and Sell Stock done

--- Practice 1/Week 1 Sequences/Best Time to Buy and Sell Stock.js
+++ Practice 1/Week 1 Sequences/Best Time to Buy and Sell Stock.js
@@ -1,5 +1,24 @@
 // Best Time to Buy and Sell Stock
 
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+// var maxProfit = function(prices) {
+    
+// };
+
+function maxProfit(prices) {
+    let margin = prices[0];
+    let sequel = 0;
+    for(let i = 1; i < prices.length; i++) {  // > was providing error
+        margin = Math.min(prices[i], margin);
+        sequel = Math.max(sequel, prices[i]-margin)        
+    }
+    return sequel
+    
+};
+
 
 
 
