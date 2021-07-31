cef1c3d865c184455dd44d334afed6f7db744055
Coder--Shona <57604500+codershona@users.noreply.github.com>
2021-07-31 11:40:03 +0000

    practicing 1

--- Practice 1/Week 1 Sequences/Best Time to Buy and Sell Stock.js
+++ Practice 1/Week 1 Sequences/Best Time to Buy and Sell Stock.js
@@ -1,5 +1,31 @@
 // Best Time to Buy and Sell Stock(Easy)
 
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+ 
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+Example 2:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+ 
+
+Constraints:
+
+1 <= prices.length <= 105
+0 <= prices[i] <= 104
+
 /**
  * @param {number[]} prices
  * @return {number}
@@ -13,7 +39,7 @@ function maxProfit(prices) {
     let sequel = 0;
     for(let i = 1; i < prices.length; i++) {  // > was providing error
         margin = Math.min(prices[i], margin);
-        sequel = Math.max(sequel, prices[i]-margin)        
+        sequel = Math.max(sequel, prices[i] - margin)        
     }
     return sequel
     
@@ -21,5 +47,17 @@ function maxProfit(prices) {
 
 
 
+Explanation: 
+Solution 1:
+We had a function which name is maxProfit and has (prices) parameters. Now If we want to execute the code, we can call the fuction using let local variables to return the value with margin and sequel value. This margin value has proces with 0 array and the sequel has only 0 numbers and strings. Now I am using for loop method where it has let variable with statement 1 with i equal 1. 
 
+Statement i - 1 is to executed (one time) before the execution of the code block. Statement 1 sets a variable before the loop starts (let i = 0).
 
+Next, Statement 2 defines the condition for the loop to run (i must be less than prices.length).
+
+Finally, Statement 3 increases a value (i++) each time the code block in the loop has been executed. 
+
+In the second point, MAth is a golobal object, which Simulate a Max Method on Arrays.
+
+If we wan to find the smallest number (in a list of numbers) using the Math.min() method, now margin store the values using Math.min to find lowset value with i prices arrays and margin. On the other hand we canuse sequel values to store and find the maximum vakue using Math.min whuch has sequel, prices ith i array that substract the margin value. 
+As a result it returns, the sequel value. 
--- Practice 1/Week 1 Sequences/Valid Anagram.js
+++ Practice 1/Week 1 Sequences/Valid Anagram.js
@@ -1,4 +1,56 @@
 // Valid Anagram (Easy)
 
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+ 
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+ 
+
+Constraints:
+
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.
+ 
+
+Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+
+function isAnagram (s, t) {
+    let odd = s.split("").sort().join();
+    let binary = t.split("").sort().join();
+    return odd === binary;
+    
+};
+
+// Or, ES6 method using With Arrow Function:
+const isAnagram = (s, t) => {
+    let odd = s.split("").sort().join();
+    let binary = t.split("").sort().join();
+    return odd === binary;
+    
+};
+
+
+
+Explanation: 
+Solution 1:
+
+
+
+
+
 
 
