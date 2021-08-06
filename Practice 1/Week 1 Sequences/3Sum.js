0b1d1b5447ca9fe834a7fea4833c2fd785b9913c
Coder--Shona <57604500+codershona@users.noreply.github.com>
2021-08-06 18:06:18 +0000

    3sum problem solved only explanation left

--- Practice 1/Week 1 Sequences/3Sum.js
+++ Practice 1/Week 1 Sequences/3Sum.js
@@ -29,6 +29,42 @@ Constraints:
 Solution:
 
 
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+
+const threeSum = (nums) => {
+    nums.sort((x, y) => x - y);
+    // must add => (arrow function or it will show that "Syntax Error: missing ) after argument list" )
+    const output = [];
+    
+    // using for loop method
+    for (let i = 0; i < nums.length; i++) {
+        let smaller = i + 1, higher = nums.length - 1, sum_total = 0;
+        // using while---if--else if statement
+        while (smaller < higher) {
+            sum_total = nums[i] + nums[smaller] + nums[higher];
+            // using if statement
+            if (sum_total === 0) {
+                // now we need to use push method
+                output.push([nums[i], nums[smaller], nums[higher]]);
+                while(nums[smaller + 1] === nums[smaller]) smaller++;
+                while(nums[higher - 1] === nums[higher]) higher--;
+                smaller++;
+                higher--;         
+            } else if (sum_total < 0) smaller++
+             else higher--;
+            // while(nums[i + 1] === nums[i]) i++;   
+        }
+        while(nums[i + 1] === nums[i]) i++;
+          // return output;
+    }
+    return output;
+};
+
+console.log(threeSum);
+
 
 Explanation:
 
