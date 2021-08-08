c0a7f60325316d25c3dac4a93ed1dcf232c177e0
Coder--Shona <57604500+codershona@users.noreply.github.com>
2021-08-08 19:58:18 +0000

    2 explanation added

--- Practice 1/Week 1 Sequences/Maximum Product Subarray.js
+++ Practice 1/Week 1 Sequences/Maximum Product Subarray.js
@@ -52,6 +52,7 @@ console.log(maxProduct);
 
 
 EXPLANATION:
+Here, we are using const variable where it has maxProduct object withnums parameter declaring arrow functions. We can noe execute where it has local variable that declares the function to access with let variable that has output object equal substraction Number.MAX_VALUE to represent the property represents the maximum numeric value representable. Then it has minimumArray with equal 1 and maximumArray with array 1. Next using for...of method where the let numberVariable as variable of the nums parameter iterations. Now we can execute with the two Array items that has minimumArray and maximumArray. Then we had used Math object to allow Math.min() and Math.max() can be used to find the lowest or highest value in a list of arguments. In Math.min it has three parameters to One or more numbers to compare. n1 is a number that compare the numberVariable parameter with n2 of minimumArray Multiplication with numberVariable and maximumArray parameters that has Multiplication with numberVariable parameters. Then using comma operator then we can use Math.max as in same process like Math.min to declare in array elements. After that, we can use output object equal Math.max() method which has output and maximumArray with two parameters. Then we can return output.
 
 
 
