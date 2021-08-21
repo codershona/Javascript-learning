38a64a8e11612d747be31d459b7be5c9927b95ad
Coder--Shona <57604500+codershona@users.noreply.github.com>
2021-08-21 19:11:45 +0000

    working on hackerrank problem solving

---
+++ HACKERRANK/30 days javascript challenge/All days.js
@@ -0,0 +1,20 @@
+// Day 1:
+
+
+function greeting(parameterVariable) {
+    // This line prints 'Hello, World!' to the console:
+    console.log('Hello, World!');
+    console.log(parameterVariable);
+
+    // Write a line of code that prints parameterVariable to stdout using console.log:
+    
+}
+
+
+// Data Type 	-----> string	
+// Parameter	 -----> parameterVariable
+// Description ====> 	A single line of text containing one or more space-separated words.
+
+
+// Day 2:
+
