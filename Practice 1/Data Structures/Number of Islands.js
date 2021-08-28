45a69b3a1f84a98c50aeb71476d765bae052c52e
Coder--Shona <57604500+codershona@users.noreply.github.com>
2021-08-28 16:03:32 +0000

    needs to solve Remove Nth Node From End of List

--- Practice 1/Data Structures/Number of Islands.js
+++ Practice 1/Data Structures/Number of Islands.js
@@ -38,7 +38,41 @@ grid[i][j] is '0' or '1'.
 
 SOLUTION:
 // JavaScript DFS (DEPTH FIRST SEARCH)
-
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+const numIslands = (grid) => {
+    let height = grid.length;
+    let weight = height && grid[0].length;
+    let countOne = 0;
+    // using for loop method
+    for(let x = 0; x < height; x++) {
+        // using for loop method
+        for(let y = 0; y < weight; y++) {
+            // using if statement
+            if (grid[x][y] === '0') 
+            continue;
+            countOne++;
+            depthFirstSearch(x, y);            
+        }
+    }
+    return countOne;
+    
+    function depthFirstSearch(x, y) {
+        // if conditions
+        if(x < 0 || y < 0 || x === height || y === weight)
+            return;
+        if (grid[x][y] === '0') 
+            return;
+        grid[x][y] = '0';
+        depthFirstSearch(x - 1, y);
+        depthFirstSearch(x + 1, y);
+        depthFirstSearch(x, y - 1);
+        depthFirstSearch(x, y + 1);
+    }
+    
+};
 
 
 
