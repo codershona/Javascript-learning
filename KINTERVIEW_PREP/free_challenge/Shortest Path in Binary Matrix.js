19fab1953574114c1272d413937dd607aeba6ed0
Coder--Shona <57604500+codershona@users.noreply.github.com>
2022-01-24 16:29:24 +0000

    Shortest Path in Binary Matrix done

--- KINTERVIEW_PREP/free_challenge/Shortest Path in Binary Matrix.js
+++ KINTERVIEW_PREP/free_challenge/Shortest Path in Binary Matrix.js
@@ -29,13 +29,80 @@ Output: -1
 
 SOLUTION:
 
-
 /**
+ * K-Interview Prep
  * BFS
  * * Time Complexity: O(N)
  * * Space Complexity: O(N)
  */
 // Runtime: 108 ms, faster than 86.30% of JavaScript online submissions for Shortest Path in Binary Matrix.
 // Memory Usage: 45.5 MB, less than 96.71% of JavaScript online submissions for Shortest Path in Binary Matrix.
+
+/* 
+  * === means strict equal
+  * q means queue
+  * n 
+  * for of loop methods to pass the value of the iterable objects
+  * && measn Logical AND
+  * || measn Logical OR
+  * !== means strict unequal
+  * < means less than
+  * >= means greater than or, equal
+  * 
+*/
+
+const shortestPathBinaryMatrix = (grid) => {
+    const order = [
+        [1, 0],
+        [-1, 0],
+        [0, 1],
+        [0, -1],
+        [1, 1],
+        [1, -1],
+        [-1, 1],
+        [-1, -1]
+    ];
+    
+    // if conditions 1
+    if (grid[0][0] === 1) 
+        return -1;
+    
+    // declaring variable 
+    const n = grid.length;
+    const q = [[0, 0, 1]];
+    // using while condition where q.length is queue conditions
+    while(q.length) {
+        let [rows, column, pass] = q.shift();
+        
+        // if conditions 2
+        if (rows === n - 1 && column === n - 1) 
+            return pass;
+        
+        // using for of loop statement loops through the values of an iterable object
+        for(let [orderX, orderY] of order) {
+            let i = rows + orderX;
+            let j = column + orderY;
+            
+            // if invalid continue
+            if (i < 0 || i >= n) continue;
+            if (j < 0 || j >= n) continue;
+            if (grid[i][j] !== 0) continue;
+            
+            q.push([i, j, pass + 1]);
+            grid[i][j] = 1;             
+        }                        
+    }    
+    return -1;
+};
+
+# Time Complexity: O(N)
+# Space Complexity: O(N)
+
+Runtime: 113 ms
+Memory Usage: 45.7 MB
+
+Runtime: 108 ms, faster than 92.20% of JavaScript online submissions for Shortest Path in Binary Matrix.
+
+Memory Usage: 45.7 MB, less than 94.27% of JavaScript online submissions for Shortest Path in Binary Matrix.
 
 
