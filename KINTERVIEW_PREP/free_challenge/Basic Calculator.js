0d98693cb7add9834d99a1e0ae3ea1c5f90fef48
Coder--Shona <57604500+codershona@users.noreply.github.com>
2022-01-27 13:39:50 +0000

    basic calculator 2 done

--- KINTERVIEW_PREP/free_challenge/Basic Calculator.js
+++ KINTERVIEW_PREP/free_challenge/Basic Calculator.js
@@ -41,3 +41,47 @@ Space Compleity : O(n)
 The space complexity of it is O(n) because the load in the stack is proportional in length of the input.
 
 
+const calculate = (s) => {
+    let result = 0;
+    let str = 1;
+    let stack = [];
+    let eval = [];
+    
+    // using for loop method
+    for (let i = 0; i < s.length; i++) {
+        let curStr = s.charAt(i);
+        // if/else if conditions
+        if (curStr === '') {
+            continue;
+        } else if (curStr === '+') {
+            str = 1;
+        } else if (curStr === '-') {
+            str = -1;
+        } else if (curStr >= '0' && curStr <= '9') {
+            let digit = curStr;
+            // using while conditions
+            while (i + 1 < s.length && s.charAt(i + 1) >= '0' && s.charAt(i + 1) <= '9') {
+                digit += s.charAt(i + 1);
+                i++;
+            }
+            result += str * parseInt(digit);
+        } else if (curStr === '(') {
+            stack.push(result);
+            result = 0;
+            eval.push(str);
+            str = 1;
+        } else if (curStr === ')') {
+            result = eval.pop() * result + stack.pop();
+            str = 1;
+        }                        
+    }
+    return result;
+    
+};
+
+
+console.log(calculate);
+console.log(calculate("1 + 1"));
+// console.log('(1+(4+5+2)-3)+(6+8'.charCodeAt(0));
+console.log(calculate(" 2-1 + 2 "));
+// console.log(calculate("(1+(4+5+2)-3)+(6+8)"));
