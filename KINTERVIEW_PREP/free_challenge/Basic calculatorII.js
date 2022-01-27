cf5a4f1018350461f6e6bdf1e596123148ce5674
Coder--Shona <57604500+codershona@users.noreply.github.com>
2022-01-27 08:35:00 +0000

    time and space analysis done

--- KINTERVIEW_PREP/free_challenge/Basic calculatorII.js
+++ KINTERVIEW_PREP/free_challenge/Basic calculatorII.js
@@ -26,3 +26,37 @@ Output: 5
 
 SOLUTION:
 
+
+Time Complexity : O(n)
+
+* The time complexity of it is O(n) because we just need to run it once.
+
+Space Complexity : O(1)
+About Space Complexity:
+
+* O(1) space complexity means that the amount of memory that you use is constant and does not depends on the data that it is processing.
+
+*  If you have a traversal algorithm for traversing the list which allocate a single pointer to do so, the traversal algorithms is considered to be of O(1) space complexity.
+
+* O(1) denotes constant space use: the algorithm allocates the same number of pointers irrespective to the list size. 
+
+* Space Complexity(s(P)) of an algorithm is the total space taken by the algorithm to complete its execution with respect to the input size. It includes both Constant space and Auxiliary space.
+
+S(P) = Constant space + Auxiliary space
+
+Constant space is the one that is fixed for that algorithm, generally equals to space used by input and local variables. 
+
+Auxiliary Space is the extra/temporary space used by an algorithm.
+
+
+* A O(1) function doesn't need to use a fixed size for all inputs, it just has to have a constant upper bound (on space) for all inputs.
+
+* For example, suppose you have a function that takes a single integer input n, and it uses 10 kB for even n and 20 kB for odd n. This function takes O(1) space, but it certainly doesn't use a fixed size. However, the upper bound is fixed, at 20 kB.
+
+
+
+
+
+
+
+
