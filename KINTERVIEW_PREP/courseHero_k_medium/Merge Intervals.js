d92ea612f87f94fb0c9f30e784bbf422b6b15f87
Coder--Shona <57604500+codershona@users.noreply.github.com>
2022-01-31 11:57:14 +0000

    Number of Islands solved

--- KINTERVIEW_PREP/courseHero_k_medium/Merge Intervals.js
+++ KINTERVIEW_PREP/courseHero_k_medium/Merge Intervals.js
@@ -1,2 +1,43 @@
 /* Merge Intervals */
 
+
+Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+
+ 
+
+Example 1:
+
+Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+Output: [[1,6],[8,10],[15,18]]
+Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+Example 2:
+
+Input: intervals = [[1,4],[4,5]]
+Output: [[1,5]]
+Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+ 
+
+
+Time Analysis :
+
+Time Complexity : O(n)
+
+Space Complexity : O(n)
+
+const merge = (intervals) => {
+    intervals.sort((x, y) => x[0] - y[0]);
+    const output = [intervals[0]];
+    // using For Of Loop method
+     for(let mergedInterval of intervals) {
+         prev_Intervals = output[output.length - 1]
+         // using if conditional statement
+         if (prev_Intervals[1] >= mergedInterval[0]) {
+             prev_Intervals[1] = Math.max(mergedInterval[1], prev_Intervals[1])
+         } else {
+         output.push(mergedInterval)
+        }             
+     } 
+      return output;
+};
+
+
--- KINTERVIEW_PREP/courseHero_k_medium/Number of Islands.js
+++ KINTERVIEW_PREP/courseHero_k_medium/Number of Islands.js
@@ -1,1 +1,70 @@
 /* Number of Islands */
+
+Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+ 
+
+Example 1:
+
+Input: grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]
+Output: 1
+Example 2:
+
+Input: grid = [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]
+Output: 3
+
+
+Time analysis :
+
+Time Complexity: O(mxn) or, O(m * n)
+
+* Complexity annotation (such as O ) describes the way an algorithm's performance (e.g. time or space) is dependent on the size of the input. It is "blind" to a specific input. When you write O(m+n) it means that the algorithm will take O(m) time when m>n and O(n) when m<n.
+
+Space Complexity : O(n) or, O(1)
+
+* To summarise the two examples above, O(1) denotes constant space use: the algorithm allocates the same number of pointers irrespective to the list size. In contrast, O(N) denotes linear space use: the algorithm space use grows together with respect to the input size.
+
+
+const numIslands = (grid) => {
+    let height = grid.length;
+    let weight = height && grid[0].length;
+    let countOne = 0;
+    // using for loop method
+    for(let x = 0; x < height; x++) {
+        // using for loop method
+        for(let y = 0; y < weight; y++) {
+            // using if statement
+            if (grid[x][y] === '0') 
+            continue;
+            countOne++;
+            depthFirstSearch(x, y);            
+        }
+    }
+    return countOne;
+    
+    function depthFirstSearch(x, y) {
+        // if conditions
+        if(x < 0 || y < 0 || x === height || y === weight)
+            return;
+        if (grid[x][y] === '0') 
+            return;
+        grid[x][y] = '0';
+        depthFirstSearch(x - 1, y);
+        depthFirstSearch(x + 1, y);
+        depthFirstSearch(x, y - 1);
+        depthFirstSearch(x, y + 1);
+    }
+    
+};
