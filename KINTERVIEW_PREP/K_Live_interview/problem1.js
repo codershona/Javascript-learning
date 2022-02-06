a10308aed9ab143b37afaf023b1bf6771745af45
Coder--Shona <57604500+codershona@users.noreply.github.com>
2022-02-06 20:04:34 +0000

    for loop method

--- KINTERVIEW_PREP/K_Live_interview/problem1.js
+++ KINTERVIEW_PREP/K_Live_interview/problem1.js
@@ -1,6 +1,4 @@
 `
-
-
 You are going on a camping trip, but before you leave you need to buy groceries. To optimize your time spent in the store, instead of buying the items from your shopping list in order, you plan to buy everything you need from one department before moving to the next.
 
 Given an unsorted list of products with their departments and a shopping list, return the time saved in terms of the number of department visits eliminated.
@@ -74,6 +72,8 @@ const list5 = ['Cheese', 'Potatoes', 'Blueberries', 'Canned Tuna'];
 
 Real Solution:
 
+// forEach method:
+
 const shopping = (products, list) => {
 //   console.log(list1);
 //   console.log(list2);
@@ -95,10 +95,38 @@ const shopping = (products, list) => {
 shopping(products, list1, list2, list3, list4, list5);
 
 
+// For Loop method
+const shopping = (products, list) => {
+  
+  for (var i = 0; i < products.length; i++){
+         console.log(products[i]);
+}
+ 
+   for (var j = 0; j < list.length; j++){
+         console.log(list[j]);
+}
+  
+  
+//   console.log(list1);
+//   console.log(list2);
+//   console.log(list3);
+//   console.log(list4);
+//   console.log(list5);
+//   console.log(products);
+  
+//   list.forEach((value, index, item) => {
+//     console.log(value, index, item);
+//     console.log(list2[5])
+//     console.log(list3[5])
+//     console.log(list1[5])
+//     console.log(list5[4])
+//     console.log(list4[4])
+//   })
+}
+// shopping(products, list1
 
 
-
-
+// Instructor solution:
 // const shopping = (products, list) => {
      
 //   list.forEach((item) => {
