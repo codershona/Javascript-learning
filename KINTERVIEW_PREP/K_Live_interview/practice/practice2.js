25fed7d90ccdae219272450b9027b577b9693173
Coder--Shona <57604500+codershona@users.noreply.github.com>
2022-02-08 09:12:23 +0000

    8 must array method

---
+++ KINTERVIEW_PREP/K_Live_interview/practice/practice2.js
@@ -0,0 +1,33 @@
+// Array method filter()
+
+
+
+// Array method map()
+
+
+
+// Array method find()
+
+
+// Array method forEach()
+
+
+
+// Array method some()
+
+
+
+
+// Array method every()
+
+
+
+
+// Array method reduce()
+
+
+
+// Array method includes()
+
+
+
