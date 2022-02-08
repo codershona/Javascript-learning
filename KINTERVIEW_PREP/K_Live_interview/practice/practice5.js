8f731a9adf0622534de8445f66973fe84733e139
Coder--Shona <57604500+codershona@users.noreply.github.com>
2022-02-08 19:21:39 +0000

    practice 5 left

--- KINTERVIEW_PREP/K_Live_interview/practice/practice5.js
+++ KINTERVIEW_PREP/K_Live_interview/practice/practice5.js
@@ -7,12 +7,38 @@ var x = [
   { name: "Carol", age: 32, gender: "F" },
   { name: "Kane", age: 20, gender: "F"},
   { name: "Jack", age: 20, gender: "M"},
-  { name: "Jeffer", age: 20, gender: "M"},
-  { name: "Mily", age: 20, gender: "F"}
+  { name: "Jeffer", age: 27, gender: "M"},
+  { name: "Mily", age: 19, gender: "F"}
 ];
 
 
 console.log(x);
+
+const filteredUser = x.filter(value => {
+  return value.age > 20
+})
+
+console.log(filteredUser);
+
+
+const findUser = x.find(value => {
+  return value.name === "Mily"
+})
+console.log(findUser);
+
+const findUser1 = x.findIndex(value => {
+  return value.name === "Mily"
+})
+console.log(findUser1);
+
+const findUser2 = x.map(value => {
+  return value.name
+})
+console.log(findUser2);
+
+const entriesUser = x[0];
+const encodeUser = Object.entries(entriesUser);
+console.log(Object.fromEntries(encodeUser));
 
 
 
