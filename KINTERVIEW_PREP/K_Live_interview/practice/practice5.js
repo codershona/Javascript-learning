c413649f5f268c8073f61cf0ce3825e81597863d
Coder--Shona <57604500+codershona@users.noreply.github.com>
2022-02-08 16:59:33 +0000

    working on it

--- KINTERVIEW_PREP/K_Live_interview/practice/practice5.js
+++ KINTERVIEW_PREP/K_Live_interview/practice/practice5.js
@@ -1,3 +1,18 @@
 // Practice 5
 
 
+var x = [
+  { name: "Jane", age: 30, gender: "F" },
+  { name: "John", age: 50, gender: "M" },
+  { name: "Carol", age: 32, gender: "F" },
+  { name: "Kane", age: 20, gender: "F"},
+  { name: "Jack", age: 20, gender: "M"},
+  { name: "Jeffer", age: 20, gender: "M"},
+  { name: "Mily", age: 20, gender: "F"}
+];
+
+
+console.log(x);
+
+
+
